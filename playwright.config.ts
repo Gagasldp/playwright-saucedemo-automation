29dc130ece90bedaa767de7d78ea12fa885ce992
Gagas Langgeng Dwi P <111678802+Gagasldp@users.noreply.github.com>
2025-08-16 19:24:29 +0700

    update: config n test result

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',              // Folder tempat test berada
-  timeout: 30000,                  // Timeout per test (ms)
+  testDir: './tests', // Folder test
+  timeout: 30000, // Timeout per test (ms)
   expect: {
-    timeout: 5000,                 // Timeout buat assertion expect()
+    timeout: 5000, // Timeout buat assertion expect()
   },
   use: {
-    baseURL: 'https://example.com', // Base URL web yang dites
-    headless: true,
+    baseURL: 'https://example.com', // Base URL
+    headless: false,
     viewport: { width: 1280, height: 720 },
+    launchOptions: {
+      slowMo: 1000 // Slow motion 500ms
+    }
   },
   reporter: [['html'], ['list']], // Reporter hasil test
 });
